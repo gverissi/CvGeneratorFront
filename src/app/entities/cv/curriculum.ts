ffb7093f546d99431c1966463d0d67821cb8e4b6
gverissi <greg.verissimo@gmail.com>
2021-05-25 21:32:01 +0200

    Add Person entity

--- src/app/entities/cv/curriculum.ts
+++ src/app/entities/cv/curriculum.ts
@@ -1,11 +1,15 @@
+import {Person} from "../person/person";
+
 export class Curriculum {
 
   id: number;
   name: string;
+  person: Person;
 
   constructor() {
     this.id = 0;
     this.name = "name";
+    this.person = new Person();
   }
 
 }
---
+++ src/app/entities/person/person.ts
@@ -0,0 +1,17 @@
+export class Person {
+
+  id: number;
+  firstName: string;
+  lastName: string;
+  job: string;
+  profile: string;
+
+  constructor() {
+    this.id = 0;
+    this.firstName = "";
+    this.lastName = "";
+    this.job = "";
+    this.profile = "";
+  }
+
+}
