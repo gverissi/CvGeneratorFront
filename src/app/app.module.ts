85ddc4c89f84547b5c2c4909d8290ced0202e2f4
gverissi <greg.verissimo@gmail.com>
2021-05-25 21:27:50 +0200

    Add navbar component and Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { CvListComponent } from './components/cv-list/cv-list.component';
 import {AngularSplitModule} from "angular-split";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: 'home', component: CvFormComponent},
+  {path: 'register', component: CvFormComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CvFormComponent,
-    CvListComponent
+    CvListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AngularSplitModule
+    AngularSplitModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
